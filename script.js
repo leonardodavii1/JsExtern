3373c9862a50d906948534608569269b5dc8ece1
Leonardo Davi D. B <sec.leonardodavi@gmail.com>
2024-07-04 20:31:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let res = document.getElementById('res')
+
+function btn1() {
+    res.innerHTML +=`<p>clicou no primeiro botão</p>`
+}
+
+function btn2() {
+    res.innerHTML += `<p>clicou no segundo botão</p>`
+}
+
+function btn3() {
+    res.innerHTML += `<p>clicou no terceiro botão</p>`
+}
+
+function btn4() {
+    res.innerHTML += `<p>clicou no quarto botão</p>`
+}
